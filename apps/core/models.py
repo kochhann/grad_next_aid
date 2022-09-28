255ad03b9e563022941dc209349f0a27295ecf0e
Juliano Kochhann <juliano@kochhann.com.br>
2022-09-28 16:26:42 -0300

    First commit

---
+++ apps/core/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Base(models.Model):
+    date_created = models.DateField('Criação', auto_now_add=True)
+    date_edited = models.DateField('Modificação', auto_now=True)
+    date_deactivated = models.DateField('Desativado', blank=True, null=True, default=None)
+    active = models.BooleanField('Ativo', default=True)
+
+    class Meta:
+        abstract = True
